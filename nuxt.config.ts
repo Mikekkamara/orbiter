3a9d525145f899102472089d99827a895d662720
Michael Kamara <mikekkamara@gmail.com>
2024-07-09 16:51:35 +0300

    vite optimize vuec, naive ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,11 +5,22 @@ import Components from "unplugin-vue-components/vite";
 import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
 
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   compatibilityDate: "2024-07-02",
   // ssr: false,
   modules: ["@nuxtjs/tailwindcss", "vue3-pixi-nuxt", "nuxtjs-naive-ui"],
+  build: {
+    transpile: process.env.NODE_ENV === "production" ? ["naive-ui", "vueuc", "@css-render/vue3-ssr", "@juggle/resize-observer"] : ["@juggle/resize-observer"],
+  },
   vite: {
+    optimizeDeps: {
+      include: process.env.NODE_ENV === "development" ? ["naive-ui", "vueuc", "date-fns-tz/esm/formatInTimeZone"] : [],
+      esbuildOptions: {
+        define: {
+          global: "globalThis",
+        },
+      },
+    },
     plugins: [
       Components({
         resolvers: [NaiveUiResolver()],
